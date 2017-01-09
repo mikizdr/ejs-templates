8a22089e98deb2d9f37146e7d8253ff5beaba482
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-01-09 18:25:10 +0100

    Added charlton products to the home page - image and desc

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var appdata = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  var myProducts = [];
+
+  myProducts = appdata.products;
+
+  /*
+  appdata.products.forEach(function(item){
+    myProducts = myProducts.concat(item.title);
+  });
+  */
+
   res.render('index', { 
     title: 'Express',
-    navbar: 'default'
+    navbar: 'default',
+    products: myProducts
   });
 });
 
